43a258449196a2fae7a35b4fd9530e1b1e591d10
Roman M. Gerasimov <48310403+GerasimovRM@users.noreply.github.com>
2019-09-01 21:21:41 +0500

    Ball::move - move ball to the value of d_x and d_y Ball::pos - return a tuple of coordinates

--- Ball.py
+++ Ball.py
@@ -2,7 +2,14 @@ from typing import Tuple
 
 
 class Ball:
-    def __init__(self, x: int, y: int, color: Tuple[int, int, int, int]):
+    def __init__(self, x: int, y: int, color: Tuple[int, int, int]) -> None:
         self.x = x
         self.y = y
         self.color = color
+
+    def move(self, d_x: int, d_y: int) -> None:
+        self.x += d_x
+        self.y += d_y
+
+    def pos(self):
+        return self.x, self.y
